014907ea85ac5a88494daee2ee464e1c1e90a5ed
VInhHuong <ngvinhhuong@gmail.com>
2022-01-15 19:03:47 +0700

    1 file added

---
+++ data.js
@@ -0,0 +1,66 @@
+const initData = [
+  {
+    name: 'anhnd2@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'huybv@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'datnt5@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'hoanghq@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'hieunt5@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'hoinv2@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'huongnv@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'lienltm@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'namdhh@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'namlh2@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'lapph@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'phongvv@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'haitv@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'tungld@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'tuyennv3@rikkeisoft.com',
+    age: 25,
+  },
+  {
+    name: 'doanvd@rikkeisoft.com',
+    age: 25,
+  },
+];
